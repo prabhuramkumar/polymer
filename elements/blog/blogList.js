17526e10b2c8c8d76d3461ba391e8b26d75b793d
prabhuramkumar <prabhu.ramkumar1@gmail.com>
2015-01-06 14:55:20 +0800

    Adding routing using flatiron

--- elements/blog/blogList.js
+++ elements/blog/blogList.js
@@ -1,34 +1,32 @@
 Polymer('blog-list', {
   accessToken: "b3a68b5a67cc88c023c8baf88211c0101258f8b9",
+  ready: function(){
+      this.$.ajax.go();
+  },
   responseChanged: function(oldValue) {
     this.blogs = this.filterMarkDown(this.response);
+    if(this.route){
+      this.loadGist();
+    }
   },
   selection: function(e){
     this.k = e.target.templateInstance.model.i;
-    this.$.pages.selected = 1;
-    var blog = e.target.templateInstance.model.blog;
-    this.blogTitle = blog.description; 
-    this.blogOwner = blog.owner.login;
-    this.blogDate= blog.updated_at; 
-    this.gistUrl = blog.url+"?access_token="+this.accessToken;
-    this.content= "empty";
+    this.route = this.k;
+    this.loadGist();
+    this.route = e.currentTarget.getAttribute('link');
+  },
+  loadGist: function(){
+    this.gistUrl = this.blogs[this.route].url+"?access_token="+this.accessToken; 
     this.$.gistAjax.go();
   },
-  transitionend: function(){
-    if (this.lastSelected) {
-      this.lastSelected = null;
-    }
+  buildBlogFromGist: function(gist){
+    this.blogTitle = gist.description; 
+    this.blogOwner = gist.owner.login;
+    this.blogDate = gist.updated_at;
+    this.blogContent = this.buildBlogContent(gist);
   },
-  back: function() {
-    this.lastSelected = this.$.pages.selected;
-    this.$.pages.selected = 0;
-    this.content= "";
-  },
-  gistresponseChanged: function(e, detail){
-    if(this.$.gistAjax.response != null){
-      this.gist = this.$.gistAjax.response;
-    }
-    var files = this.gist.files;
+  buildBlogContent: function(gist){
+    var files = gist.files;
     var contentFile;
     for (var i in files) {
         if (files.hasOwnProperty(i) && typeof(i) !== 'function') {
@@ -42,6 +40,23 @@ Polymer('blog-list', {
     else{
       this.content = "This is a empty blog";
     }
+    return this.content;
+  },
+  transitionend: function(){
+    if (this.lastSelected) {
+      this.lastSelected = null;
+    }
+  },
+  back: function() {
+    this.lastSelected = this.$.pages.selected;
+    this.route = "";
+    this.content= "";
+  },
+  gistresponseChanged: function(e, detail){
+    if(this.$.gistAjax.response != null){
+      this.gist = this.$.gistAjax.response;
+    }
+    this.buildBlogFromGist(this.gist);
   },
   filterMarkDown: function(blogs){
     this.filteredBlogs = [];
